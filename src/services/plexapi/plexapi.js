c1123b536a7b83d590e394d63be1eeadca0e4402
Christopher Caroni <christophercaroni97@gmail.com>
2019-05-08 16:56:58 +0200

    fix plexapi: pass user obj

--- src/services/plexapi/plexapi.js
+++ src/services/plexapi/plexapi.js
@@ -1,12 +1,14 @@
 import Axios from 'axios';
 import { getClientUUID } from '../device/device';
 
-export const signIn = (user, password) =>
+export const signIn = (login, password) =>
     Axios.post(
         'https://plex.tv/users/sign_in.json',
         {
-            'user[login]': user,
-            'user[password]': password,
+            user: {
+                login,
+                password,
+            },
         },
         {
             headers: {
