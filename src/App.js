f991137dca0523c435d09db4a3c74f61129574f8
Christopher Caroni <christophercaroni97@gmail.com>
2019-05-08 16:17:37 +0200

    core: setup material-ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <CssBaseline>
+            <div>TODO</div>
+        </CssBaseline>
     );
 }
 
-export default App;
+export { App };
